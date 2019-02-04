2c80ed70aaf3eb04d8f630c920b2bb629915b3e5
michael <mnc24@njit.edu>
2019-02-04 18:32:07 -0500

    Added table

--- index.php
+++ index.php
@@ -8,11 +8,48 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Cars</title>
 </head>
 <body>
 <h1>Cars</h1>
 
-<p>  this is paragraph one </p>;
-<p>  this is paragraph two </p>;
-<p>  this is paragraph three </p>;
+<p>  this is paragraph one </p>
+<p>  this is paragraph two </p>
+<p>  this is paragraph three </p>
+
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Company</th>
+        <th scope="col">Model</th>
+        <th scope="col">Cost</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>Ford</td>
+        <td>Focus</td>
+        <td>~$18,000</td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>Audi</td>
+        <td>A4</td>
+        <td>~$38,000</td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <td>BMW</td>
+        <td>3 Series</td>
+        <td>~$40,000</td>
+    </tr>
+    <tr>
+        <th scope="row">4</th>
+        <td>Acura</td>
+        <td>TLX</td>
+        <td>~$33,000</td>
+    </tr>
+    </tbody>
+</table>
